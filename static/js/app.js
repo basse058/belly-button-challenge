1d4cf930f8356417f5d215ffcfb13bae7b4cc980
basse058 <basse058@gmail.com>
2023-01-11 23:18:55 -0600

    Finished?

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
 const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
 
-function panelInfo(id) {
+function Panel(id) {
     d3.json(url).then(function (data) {
         let sampleData = data;
         let metadata = sampleData.metadata;
@@ -116,8 +116,9 @@ function init() {
         Object.values(names).forEach(value => {
             dropDown.append('option').text(value);
         })
-        panelInfo(names[0]);
-        Plots(names[0])
+        Panel(names[0]);
+        Plots(names[0]);
+        Plot(names[0])
     })
 };
 init();
